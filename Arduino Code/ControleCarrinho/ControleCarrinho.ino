5b53cc22bf9e1bff3678a0d7c73a10f339d3af76
enryson <enryson@gmail.com>
2017-09-23 00:18:33 -0300

    Update
    Adicionado o codigo do controle para o carrinho

---
+++ Arduino Code/ControleCarrinho/ControleCarrinho.ino
@@ -0,0 +1,38 @@
+#include <SoftwareSerial.h>
+int analogPin = 6; 
+String servo = "n";
+String esc = "r";
+
+
+SoftwareSerial mySerial(3, 4);
+int servoVal;
+int escVal;
+
+int val = 0;
+int va = 0;
+void setup() {
+  mySerial.begin(9600);
+  Serial.begin(9600);
+}
+
+
+void loop() {
+  servoVal = analogRead(5);          
+  servoVal = map(servoVal, 0, 1023, 64, 180); 
+  //val = analogRead(5);
+
+  escVal = analogRead(6);          
+  escVal = map(escVal, 0, 1023, 180, 0); 
+  
+  int ser = (servoVal);
+  mySerial.print(ser+servo);
+  //Serial.println(ser);
+
+  //va = analogRead(6);
+  int es = (escVal);
+  mySerial.print(es+esc);
+  
+  delay(10);
+
+}
+
