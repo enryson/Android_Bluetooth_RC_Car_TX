5fea97a3d937f4e7e37ad94a5c17a8e9952c50cd
enryson <enryson@gmail.com>
2017-12-11 01:55:35 -0200

    Updade

--- Arduino Code/CarrinhoRCBluetooth/CarrinhoRCBluetooth.ino
+++ Arduino Code/CarrinhoRCBluetooth/CarrinhoRCBluetooth.ino
@@ -1,13 +1,11 @@
 
 #include <Servo.h>
-//#include <SoftwareSerial.h>
 #include <Arduino.h>
 
 #define buser 8
-//SoftwareSerial mySerial(2, 3);
+
 Servo myservo,esc;
 
-//string
 String inString = "";
 
 unsigned long startTime;
@@ -19,60 +17,52 @@ float vPow = 5;
 float r1 = 100000.00;
 float r2 = 10000.00;
 
-
-
 int voltcheck = 0;
 
 void setup() {
-  pinMode(13, OUTPUT);
   pinMode(8,OUTPUT);
   myservo.attach(6);//Carrinho
   esc.attach(5);
   digitalWrite(13, LOW);
   Serial.begin(9600);
-  myservo.write(120);
+  myservo.write(115);
   getv();
+  tone(buser,262,200); //DO
 }
 
 void loop() {
   if (Serial.available() > 0) {
-    int inChar = Serial.read();
-    
+    int inChar = Serial.read();    
     if (isDigit(inChar)) {
       inString += (char)inChar; 
     }
-    
     if (inChar == 'n'){
       int n = inString.toInt();
       if (n <= 180){
-        //Serial.println(n);
         myservo.write(n);        
       }
       inString = "";
     }
     if (inChar == 'r') {
       int x = inString.toInt();
-      if (x <= 165){
-        //Serial.println(x);
+      if (x <= 170){
         esc.write(x);      
       }
       inString = "";
     }    
     if (inChar == 'm') {      
       if (voltcheck > 10){
-      getv();
-      voltcheck = 0;
-      }
-      else {
+        getv();
+        voltcheck = 0;
+      } else {
         voltcheck = voltcheck + 1;
       }
-      digitalWrite(13, HIGH);
       startTime = millis();
     }
   }
 }
 static void getv() {
-  float v = ((analogRead(1)+40) * vPow) / 1023.0;
+    float v = ((analogRead(1)+40) * vPow) / 1023.0;
     float v2 = v / (r2 / (r1 + r2));
     if (v2 < 10){      
       musicTone();
@@ -81,7 +71,6 @@ static void getv() {
     Serial.println(v2);
     Serial.println("v");
 }
-
 static void musicTone() {  
     delay(2000);
     tone(buser,262,200); //DO
--- Arduino Code/ControleCarrinho/ControleCarrinho.ino
+++ Arduino Code/ControleCarrinho/ControleCarrinho.ino
@@ -1,38 +1,33 @@
 #include <SoftwareSerial.h>
+SoftwareSerial mySerial(3, 4);
 int analogPin = 6; 
+
 String servo = "n";
 String esc = "r";
 
-
-SoftwareSerial mySerial(3, 4);
 int servoVal;
 int escVal;
 
 int val = 0;
 int va = 0;
-void setup() {
+
+void setup() {  
   mySerial.begin(9600);
   Serial.begin(9600);
 }
-
-
-void loop() {
-  servoVal = analogRead(5);          
-  servoVal = map(servoVal, 0, 1023, 64, 180); 
-  //val = analogRead(5);
-
-  escVal = analogRead(6);          
+void loop() { 
+  escVal = analogRead(6);  
+  servoVal = analogRead(5); 
+      
+  servoVal = map(servoVal, 0, 1023, 60, 180); 
   escVal = map(escVal, 0, 1023, 180, 0); 
   
   int ser = (servoVal);
   mySerial.print(ser+servo);
-  //Serial.println(ser);
 
-  //va = analogRead(6);
   int es = (escVal);
   mySerial.print(es+esc);
+
   
   delay(10);
-
 }
-
