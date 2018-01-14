7e079d352fb840cfcfae3bf4f76f778b4ad59530
enryson <enryson@gmail.com>
2018-01-14 20:03:05 -0200

    up

--- Arduino Code/ControleCarrinho/ControleCarrinho.ino
+++ Arduino Code/ControleCarrinho/ControleCarrinho.ino
@@ -5,29 +5,29 @@ int analogPin = 6;
 String servo = "n";
 String esc = "r";
 
-int servoVal;
-int escVal;
-
-int val = 0;
-int va = 0;
+int servoVal = 0;
+int escVal = 0;
 
 void setup() {  
   mySerial.begin(9600);
   Serial.begin(9600);
 }
 void loop() { 
-  escVal = analogRead(6);  
-  servoVal = analogRead(5); 
-      
-  servoVal = map(servoVal, 0, 1023, 60, 180); 
-  escVal = map(escVal, 0, 1023, 180, 0); 
+  escVal = analogRead(6);
+  servoVal = analogRead(4);
+  
+  servoVal = map(servoVal, 0, 1023, 90, 180);
+  //escVal = map(escVal, 0, 1023, 0, 180);
+  
+  Serial.println(escVal);
   
   int ser = (servoVal);
   mySerial.print(ser+servo);
 
-  int es = (escVal);
+  
+  int es = (escVal);  
+  Serial.println(escVal);
   mySerial.print(es+esc);
-
   
   delay(10);
 }
