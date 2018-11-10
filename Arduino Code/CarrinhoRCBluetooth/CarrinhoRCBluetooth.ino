db95aef9c02b596c106cb3b0b5e6306fcf3464d5
Enryson Oliveira Ferraz <enryson@gmail.com>
2018-11-10 11:54:41 -0200

    Update
    remove code..

--- Arduino Code/CarrinhoRCBluetooth/CarrinhoRCBluetooth.ino
+++ Arduino Code/CarrinhoRCBluetooth/CarrinhoRCBluetooth.ino
@@ -1,70 +1,39 @@
-#include <Servo.h>
+#include <PWMServo.h>
 #include <Arduino.h>
+#include <SoftwareSerial.h>
 
-#define buser 8
+PWMServo myservo,esc;
 
-Servo myservo,esc;
+SoftwareSerial mySerial(5,6); 
 
 String inString = "";
 
-unsigned long startTime;
-unsigned long otherTime;
-
-int prev = 150;
-
-float vPow = 5;
-float r1 = 100000.00;
-float r2 = 10000.00;
-
-int voltcheck = 0;
-
 void setup() {
-  pinMode(8,OUTPUT);
-  myservo.attach(6);//Carrinho
-  esc.attach(5);
-  digitalWrite(13, LOW);
-  Serial.begin(9600);
-  myservo.write(115);
-  getv();
-  //tone(buser,262,200); //DO
+  myservo.attach(9);
+  esc.attach(10);
+  mySerial.begin(38400);
+  myservo.write(122);
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    int inChar = Serial.read();    
+  if (mySerial.available() > 0) {
+    int inChar = mySerial.read();    
     if (isDigit(inChar)) {
       inString += (char)inChar; 
     }
     if (inChar == 'n'){
       int n = inString.toInt();
       if (n <= 180){
-        myservo.write(n);        
+        myservo.write(n);   
       }
       inString = "";
     }
     if (inChar == 'r') {
       int x = inString.toInt();
       if (x <= 170){
-        esc.write(x);      
+        esc.write(x);
       }
       inString = "";
-    }    
-    if (inChar == 'm') {      
-      if (voltcheck > 10){
-        getv();
-        voltcheck = 0;
-      } else {
-        voltcheck = voltcheck + 1;
-      }
-      startTime = millis();
     }
   }
 }
-static void getv() {
-    float v = ((analogRead(1)+40) * vPow) / 1023.0;
-    float v2 = v / (r2 / (r1 + r2));
-    Serial.println("{");
-    Serial.println(v2);
-    Serial.println("v");
-}
-
